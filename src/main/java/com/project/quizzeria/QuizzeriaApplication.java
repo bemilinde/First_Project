2a4d3f039715655b8493bfb653f98a4e0aea204c
bemilinde <bemilinde@gmail.com>
2023-05-28 21:06:09 +0900

    First Commit

---
+++ src/main/java/com/project/quizzeria/QuizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.project.quizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzeriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzeriaApplication.class, args);
+    }
+
+}
